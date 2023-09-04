47c14a395e33a0e9e0a433f087ec476bcd8526eb
Kaosc <themelihbt@gmail.com>
2023-09-04 13:20:12 +0300

    mui setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-function App() {
+import Navbar from "./components/Navbar"
+
+export default function App() {
 	return (
-		<main className="bg-gradient-to-r from-zinc-950 to-zinc-900 px-3">
+		<main className="bg-gradient-to-r from-zinc-950 to-zinc-900">
+			<Navbar />
 			<p className="text-2xl text-center text-white font-bold">HOME</p>
 		</main>
 	)
 }
-
-export default App
