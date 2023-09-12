9abea4365aab8062fabb06f738c49eca8562f037
Kaosc <themelihbt@gmail.com>
2023-09-13 02:00:00 +0300

    update

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,13 +4,19 @@ import { IoMdSettings } from "react-icons/io"
 import { IoIosAdd } from "react-icons/io"
 import { AiOutlineFolderOpen } from "react-icons/ai"
 import { BiBookmarkPlus } from "react-icons/bi"
+import { LiaFileImportSolid } from "react-icons/lia"
 
 import Divider from "./ui/Divider"
 import SearchBar from "./SearchBar"
 import BookmarkForm from "./form/BookmarkForm"
 import GroupForm from "./form/GroupForm"
+import { useDispatch } from "react-redux"
+import { addBookmark } from "../redux/features/bookmarkSlice"
+import { nanoid } from "nanoid"
+import { faviconPlaceHolder } from "../utils/constants"
 
 export default function Navbar() {
+	const dispatch = useDispatch()
 	const [bookmarkFormVisible, setBookmarkFormVisible] = useState(false)
 	const [groupFormVisible, setGroupFormVisible] = useState(false)
 
@@ -18,9 +24,26 @@ export default function Navbar() {
 		if (groupFormVisible) setGroupFormVisible(false)
 		setBookmarkFormVisible((prev) => !prev)
 	}
+
 	const handleGroupFormVisible = () => {
 		if (bookmarkFormVisible) setBookmarkFormVisible(false)
 		setGroupFormVisible((prev) => !prev)
+	}
+
+	const addActiveTabToBookmark = async () => {
+		let activeTab = await chrome.tabs.query({ active: true, currentWindow: true })
+
+		if (!activeTab[0]?.url) return
+
+		dispatch(
+			addBookmark({
+				id: nanoid(),
+				favicon: activeTab[0]?.favIconUrl || faviconPlaceHolder,
+				title: activeTab[0]?.title || "Untitled",
+				url: activeTab[0]?.url,
+				groupId: "default",
+			})
+		)
 	}
 
 	const texts = {
@@ -43,6 +66,15 @@ export default function Navbar() {
 				</div>
 
 				<div className="flex items-center justify-center">
+					<button
+						className="flex items-center justify-center hover:opacity-50 hover:animate-pulse mr-1"
+						onClick={addActiveTabToBookmark}
+					>
+						<LiaFileImportSolid
+							size={23}
+							className="text-white"
+						/>
+					</button>
 					<div className="group">
 						<button className="flex items-center justify-center hover:opacity-50 hover:animate-pulse mr-1">
 							<IoIosAdd
